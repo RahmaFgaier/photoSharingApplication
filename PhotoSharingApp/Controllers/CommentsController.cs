5ca308cafe19cb2be6056c5b1efb48835d3105f6
RahmaFgaier <rahma.fgaier@etudiant-enit.utm.tn>
2018-05-19 00:34:59 +0100

    add Comment int, controller

---
+++ PhotoSharingApp/Controllers/CommentsController.cs
@@ -0,0 +1,20 @@
+﻿using PhotoSharingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class CommentsController : Controller
+    {
+        ICommentRepository commentRepository = new CommentRepository; 
+        public ActionResult DisplayCommentsForPhoto (int PhotoID)
+        {
+            //Use the repository to get the comments
+            ICollection<Comment> comments = commentRepository.GetComments(PhotoID);
+            return View("DisplayComments", comments);
+        }
+    }
+}
